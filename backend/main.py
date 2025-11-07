bc916fcc3627e90de750ab404c8b5a11af75f806
KONIGANTI ROHITHREDDY <konugantisairohithreddy@gmail.com>
2025-11-07 11:34:36 +0530

    Remove wildcard from allowed origins in CORS
    Restrict allowed origins for CORS configuration.

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,6 @@ app.add_middleware(
         "https://ai-job-navigator.firebaseapp.com",
         ],  # Your React frontend
     allow_credentials=True,
-    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
